96b753c974a1c3e1beb9e74c9b191b0b4c28f417
gierMann <thisisgie.21@gmail.com>
2020-04-29 14:11:58 +0700

    UPDATE: added masthead structure and style for desktop , added templated mixins , functions, variables

---
+++ components/masthead.php
@@ -0,0 +1,17 @@
+<section class="section section--masthead-container">
+    <div class="masthead-wrapper" style="background-image: url('/assets/img/masthead.jpg')">
+        <div class="header-container">
+            <div class="header-wrapper">
+                <div class="header-title-wrapper">
+                    <h1>Starnight App</h1>
+                </div>
+                <div class="header-description-wrapper">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio omnis officiis fuga quasi inventore qui accusantium maxime sint veritatis ullam.</p>
+                </div>
+            </div>
+            <div class="get-started-btn-wrapper">
+                <button class="btn btn-start">Get Started</button>
+            </div>
+        </div>
+    </div>
+</section>
