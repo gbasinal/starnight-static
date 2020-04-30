d11cb0362aa3bb5ae9c6c85c8937879895d16b5c
gierMann <thisisgie.21@gmail.com>
2020-04-30 21:54:49 +0700

    UPDATE: added divider in two col component, adjusted width per item in 991 tablet view

--- components/two-col.php
+++ components/two-col.php
@@ -26,6 +26,8 @@
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Similique saepe enim tempore dolorem reiciendis ab accusantium ea porro vitae nesciunt iure quas reprehenderit nemo, voluptas eum amet harum possimus quaerat!</p>
                 </div>
             </div>
+
+            <div class="divider"></div>
         </div>
         <div class="item-btn-container">
             <button class="btn btn-try">Try Demo</button>
